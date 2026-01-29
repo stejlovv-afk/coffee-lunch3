a1ba2e9d1117e1cfde2ec43355ebcb71b6c05d36
stejlovv-afk <stejlovv@gmail.com>
2026-01-29 03:46:30 +0300

    Update vite-env.d.ts

--- vite-env.d.ts
+++ vite-env.d.ts
@@ -8,3 +8,34 @@ interface ImportMetaEnv {
 interface ImportMeta {
   readonly env: ImportMetaEnv
 }
+
+interface Window {
+  Telegram?: {
+    WebApp?: {
+      ready: () => void;
+      expand: () => void;
+      setHeaderColor: (color: string) => void;
+      setBackgroundColor: (color: string) => void;
+      enableClosingConfirmation: () => void;
+      initDataUnsafe?: {
+        user?: {
+          username?: string;
+          first_name?: string;
+        };
+      };
+      HapticFeedback?: {
+        notificationOccurred: (type: 'error' | 'success' | 'warning') => void;
+        impactOccurred: (style: 'light' | 'medium' | 'heavy' | 'rigid' | 'soft') => void;
+      };
+      MainButton: {
+        setText: (text: string) => void;
+        textColor: string;
+        color: string;
+        isVisible: boolean;
+        onClick: (cb: () => void) => void;
+        offClick: (cb: () => void) => void;
+      };
+      sendData: (data: string) => void;
+    };
+  }
+}
